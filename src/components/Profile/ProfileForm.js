37d7c3bc8ec314a47cdbd6f2d5a88e869de1cb79
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2026-05-11 23:39:23 +0530

    update pass done

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,74 @@
+import { useRef, useContext } from 'react';
+
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+  const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword =
+      passwordInputRef.current.value;
+
+    try {
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            idToken: authCtx.token,
+            password: enteredNewPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(
+          data.error.message || 'Failed to change password'
+        );
+      }
+
+      console.log(data);
+
+      alert('Password changed successfully!');
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
   return (
-    <form className={classes.form}>
+    <form
+      className={classes.form}
+      onSubmit={submitHandler}
+    >
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor='new-password'>
+          New Password
+        </label>
+
+        <input
+          type='password'
+          id='new-password'
+          minLength='7'
+          ref={passwordInputRef}
+        />
       </div>
+
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
